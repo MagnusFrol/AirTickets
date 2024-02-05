f5ab994f35786b2f35663af19b5963993d84d6e4
MagnusFrol <magnusfrol@gmail.com>
2024-02-06 02:51:25 +0300

    Create Project2

--- src/main/java/ru/demo/Main.java
+++ src/main/java/ru/demo/Main.java
@@ -1,7 +1,15 @@
 package ru.demo;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) throws IOException, ParseException {
+        Object obj = new JSONParser().parse(new FileReader("c:/tickets.json"));
+        JSONObject jo = (JSONObject) obj;
+        JSONArray ticketsArr = (JSONArray) jo.get("tickets");
+        Iterator ticketsItr = ticketsArr.iterator();
+        System.out.println("tickets:");
+
+        while (ticketsItr.hasNext()) {
+            JSONObject test = (JSONObject) ticketsItr.next();
+            System.out.println("- tickets: " + test.get("departure_time") );
+        }
 }
