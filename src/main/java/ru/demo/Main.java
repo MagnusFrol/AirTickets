f1a38b4af32f9ba44984d69a5540aa859214c0ca
MagnusFrol <magnusfrol@gmail.com>
2024-02-26 04:46:07 +0300

    add JSON

--- src/main/java/ru/demo/Main.java
+++ src/main/java/ru/demo/Main.java
@@ -1,8 +1,19 @@
-package ru.demo;
+
+package jason_help;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
-        Object obj = new JSONParser().parse(new FileReader("c:/tickets.json"));
+        Object obj = new JSONParser()
+                .parse(new FileReader("C:\\tickets.json"));
         JSONObject jo = (JSONObject) obj;
         JSONArray ticketsArr = (JSONArray) jo.get("tickets");
         Iterator ticketsItr = ticketsArr.iterator();
@@ -10,8 +21,7 @@ public class Main {
 
         while (ticketsItr.hasNext()) {
             JSONObject test = (JSONObject) ticketsItr.next();
-            System.out.println("- tickets: " + test.get("departure_time"));
-
+            System.out.println("- tickets: " + test.get("departure_time") );
         }
-
     }
+}
